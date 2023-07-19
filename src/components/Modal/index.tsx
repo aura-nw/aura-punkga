b3bb5bb8bac22ced86ffde3dd0a186d9626d2d3b
imhson <phamhoangson99@gmail.com>
2023-07-19 14:46:54 +0700

    add x

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,5 @@
 import { Dialog, Transition } from "@headlessui/react"
+import { XMarkIcon } from "@heroicons/react/24/outline"
 import { Fragment, useRef } from "react"
 
 export default function Modal({ open, setOpen, children }) {
@@ -28,6 +29,9 @@ export default function Modal({ open, setOpen, children }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8">
+                <div className="absolute right-2 top-2 cursor-pointer text-gray-600" onClick={() => setOpen(false)}>
+                  <XMarkIcon className="w-5 h-5" />
+                </div>
                 {children}
               </Dialog.Panel>
             </Transition.Child>
