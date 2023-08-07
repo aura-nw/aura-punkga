7617563b2804f35cb3308ca53467094c950f20a5
imhson <phamhoangson99@gmail.com>
2023-08-07 17:28:04 +0700

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -109,7 +109,7 @@ export default function Header({}) {
           isSearchFocused ? 'z-20 opacity-25' : '-z-20 opacity-0'
         }`}></div>
       <header
-        className='border-b-2 border-light-gray border-solid sticky w-[100vw] top-0 z-50 transition-all duration-300 backdrop-blur-[15px] !bg-transparent
+        className='border-b-2 border-light-gray border-solid sticky w-full overflow-hidden top-0 z-50 transition-all duration-300 backdrop-blur-[15px] !bg-transparent
         '>
         <nav className='md:hidden pk-container p-3'>
           <div className='flex justify-between items-center gap-2'>
