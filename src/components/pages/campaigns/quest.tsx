659618d72f48acb3ee27b1fc9888bb3d6cbc48ff
imhson <phamhoangson99@gmail.com>
2023-10-23 10:46:45 +0700

    fix link

--- src/components/pages/campaigns/quest.tsx
+++ src/components/pages/campaigns/quest.tsx
@@ -56,7 +56,7 @@ export default function Quest({ data }: { data: Quest }) {
       ? `/comic/${(data.requirement.comment || data.requirement.read).manga?.slug}/chapter/${
           (data.requirement.comment || data.requirement.read).chapter?.number
         }`
-      : `/comic/${data.requirement.subscribe.manga?.slug}`
+      : `/comic/${data.requirement.subscribe.manga?.slug}/chapter/1`
   if (!data.unlock)
     return (
       <>
