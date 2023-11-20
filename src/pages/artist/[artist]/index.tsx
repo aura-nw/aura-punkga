e14afeaab733d8c71c983246a31b202a7e8222ac
imhson <phamhoangson99@gmail.com>
2023-11-20 10:39:05 +0700

    fix seo artist

--- src/pages/artist/[artist]/index.tsx
+++ src/pages/artist/[artist]/index.tsx
@@ -1,31 +1,42 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { compose } from 'ramda'
 import Artist from './artist'
 import withApi from './with-api'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { i18n } from 'next-i18next'
+import HeadComponent from 'components/Head'
 const ComposedArtist = compose(withApi)(Artist)
 
 export default function Page(props) {
   if (props.justHead || props.pageProps?.justHead) {
-    return <></>
+    return <HeadComponent data={props.pageProps?.metadata || props.metadata} />
   }
-  return <ComposedArtist />
+  return (
+    <>
+      <HeadComponent data={props.pageProps?.metadata || props.metadata} />
+      <ComposedArtist />
+    </>
+  )
 }
-
-export const getStaticProps = async ({ locale }) => {
-  if (process.env.NODE_ENV === 'development') {
-    await i18n?.reloadResources()
-  }
-  return {
-    props: {
-      ...(await serverSideTranslations(locale!, ['common'])),
-    },
-  }
-}
-
-export const getStaticPaths = ({ locales }) => {
-  return {
-    paths: [],
-    fallback: true,
+export const getServerSideProps = async (context) => {
+  if (context.params?.artist) {
+    const res = await fetch(`https://api.punkga.me/creator/${context.params?.artist}`)
+    const data = await res.json()
+    const creator = data?.data?.creators?.[0]
+    if (!creator)
+      return {
+        props: {
+          ...(await serverSideTranslations(context?.locale!, ['common'])),
+        },
+      }
+    const props = {
+      image: creator.avatar_url,
+      title: creator.pen_name,
+      description: creator.bio,
+    }
+    return {
+      props: {
+        metadata: props,
+        ...(await serverSideTranslations(context?.locale!, ['common'])),
+      },
+    }
   }
 }
