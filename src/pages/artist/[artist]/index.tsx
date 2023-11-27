4413ede69155fafeafec92ba54ad36ce908718e6
imhson <phamhoangson99@gmail.com>
2023-11-27 14:44:43 +0700

    update seo

--- src/pages/artist/[artist]/index.tsx
+++ src/pages/artist/[artist]/index.tsx
@@ -16,6 +16,43 @@ export default function Page(props) {
     </>
   )
 }
+const authorSeo = [
+  {
+    slug: 'tien_tran_rb',
+    description:
+      "A 17-year-old artist with over 10 years of comic drawing experience. She has drawn the 'HAMULAGE' comic - a compelling psychological story about Brenda and her quest for ancient power.",
+  },
+  {
+    slug: 'moffi',
+    description:
+      "With one year of comic drawing experience, MOFFI introduces to PunkgaMe the comic 'Neon force' - exploring powers from 'punk' Chip and the Neon Force's battle against darkness.",
+  },
+  {
+    slug: 'tate',
+    description:
+      "Study abroad in Australia, presenting a Cyberpunk world of body mods and 'punkchips' for superpowers. Bill, unable to use them, dreams of their mystical powers.",
+  },
+  {
+    slug: 'phat_bear',
+    description:
+      "With 8 years of comic-creating experience, taking PunkgaMe readers on a journey to explore the primitive world of Hero Cyber, battling against Yukio Sasaki's blood-soaked Heaven",
+  },
+  {
+    slug: 'hanz',
+    description:
+      "With over 10 years of comic creating, the author of 'Hero Cyber' - a journey of an orphan boy who becomes a hacker by night, whose life changes after meeting a mysterious girl.",
+  },
+  {
+    slug: 'howater',
+    description:
+      'Drawing comics since age 7, author of 3 held comics, passionate about Sennen, Shounen, ... Bringing the tale of Young Hero Pan JR and his companion.',
+  },
+  {
+    slug: 'uma',
+    description:
+      "Passionate about movies, reading, and sports. Motto: Art isn't just for money. Presenting the story of 'Spice' and the team, uncovering dark secrets in Revo.",
+  },
+]
 export const getServerSideProps = async (context) => {
   if (context.params?.artist) {
     const res = await fetch(`https://api.punkga.me/creator/${context.params?.artist}`)
@@ -30,7 +67,10 @@ export const getServerSideProps = async (context) => {
     const props = {
       image: creator.avatar_url,
       title: creator.pen_name,
-      description: creator.bio,
+      description:
+        context?.locale == 'en'
+          ? authorSeo.find((author) => context.params?.artist.includes(author.slug)).description
+          : creator.bio,
       canonical: `https://punkga.me/artist/${context.params?.artist}`,
     }
     return {
