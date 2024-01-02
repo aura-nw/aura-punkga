a08e9d29a070a40c23c5a571b2f8bf51a1b5d886
imhson <phamhoangson99@gmail.com>
2024-01-02 14:55:33 +0700

    update stag

--- src/pages/campaigns/[campaignSlug]/quest.tsx
+++ src/pages/campaigns/[campaignSlug]/quest.tsx
@@ -1,19 +1,33 @@
-import Modal from 'components/Modal';
-import { useState } from 'react';
-import { Quest } from 'src/models/campaign';
+import Modal from 'components/Modal'
+import { useState } from 'react'
+import { Quest } from 'src/models/campaign'
 
 export default function QuestItem({ quest }: { quest: Quest }) {
-  console.log(quest)
+  if (quest.type == 'Read') {
+    return <ReadingQuest data={quest} />
+  }
   return <div></div>
 }
 function ReadingQuest({ data }: { data: Quest }) {
   const [open, setOpen] = useState(false)
-  return <>
-    <Modal open={open} setOpen={setOpen} >
-      abc
-    </Modal>
-    <div>
-      abnc
-    </div>
-  </>
+  return (
+    <>
+      <Modal open={open} setOpen={setOpen}>
+        abc
+      </Modal>
+      <div className='bg-[#F2F2F2] rounded-[10px] p-4 flex gap-[10px]'>
+        <div className='flex-1'>
+          <div className='text-xs leading-[15px] font-bold min-h-[32px]'>
+            {data.repeat == 'Daily' && (
+              <span className='bg-[#E2D8FF] text-[#A247FF] font-bold rounded-[3px] px-2 pb-[1px] text-[10px] leading-[13px] mr-[5px]'>
+                Daily
+              </span>
+            )}
+            {data.name}
+          </div>
+        </div>
+        <div className='w-[140px] shrink-0'></div>
+      </div>
+    </>
+  )
 }
