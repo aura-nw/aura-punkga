a06aa6ddcb7b1b0354757335779f41deefa13167
imhson <phamhoangson99@gmail.com>
2024-01-30 16:03:51 +0700

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -48,7 +48,7 @@ export default function Footer() {
               </div>
             </div>
             <div className='hidden items-center gap-6 md:flex'>
-              <Link href='https://www.facebook.com/PunkgaMeManga/'>
+              <Link target='_blank' href='https://www.facebook.com/PunkgaMeManga/'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='32' height='33' viewBox='0 0 32 33' fill='none'>
                   <circle cx='16' cy='16.8906' r='14' fill='#23FF81' />
                   <path
@@ -57,7 +57,7 @@ export default function Footer() {
                   />
                 </svg>
               </Link>
-              <Link href='https://twitter.com/PunkgaMeManga'>
+              <Link target='_blank' href='https://twitter.com/PunkgaMeManga'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32' fill='none'>
                   <path
                     d='M2.06813 3L12.8786 17.4544L2 29.2065H4.44853L13.9729 18.9171L21.6681 29.2065H30L18.581 13.9392L28.7069 3H26.2584L17.4872 12.476L10.4 3H2.06813ZM5.6688 4.80339H9.49643L26.3989 27.4031H22.5712L5.6688 4.80339Z'
@@ -65,7 +65,7 @@ export default function Footer() {
                   />
                 </svg>
               </Link>
-              <Link href='https://www.youtube.com/@PunkgaMeManga'>
+              <Link target='_blank' href='https://www.youtube.com/@PunkgaMeManga'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='32' height='33' viewBox='0 0 32 33' fill='none'>
                   <path
                     d='M2.24451 10.8317C2.37304 8.85296 3.96395 7.30219 5.94447 7.20407C8.81239 7.06199 12.9115 6.89062 16 6.89062C19.0885 6.89062 23.1876 7.06199 26.0555 7.20407C28.0361 7.30219 29.627 8.85296 29.7555 10.8317C29.8786 12.7276 30 15.0603 30 16.8906C30 18.721 29.8786 21.0537 29.7555 22.9495C29.627 24.9283 28.0361 26.4791 26.0555 26.5772C23.1876 26.7193 19.0885 26.8906 16 26.8906C12.9115 26.8906 8.81239 26.7193 5.94447 26.5772C3.96395 26.4791 2.37304 24.9283 2.24451 22.9495C2.12136 21.0537 2 18.721 2 16.8906C2 15.0603 2.12136 12.7276 2.24451 10.8317Z'
@@ -78,7 +78,7 @@ export default function Footer() {
           </div>
           <div className='flex flex-col gap-5'>
             <div>
-              <div className='text-2xl leading-[38px] text-[#C8C8C8] capitalize font-bold lg:text-5xl lg:leading-[54px]'>
+              <div className='text-2xl leading-[38px] text-[#C8C8C8] font-bold lg:text-5xl lg:leading-[54px]'>
                 {t('Never miss a drop')}
               </div>
               <div className='text-sm leading-[18px] text-[#ABABAB]'>{t('Subscribe to get fresh news update')}</div>
@@ -110,7 +110,7 @@ export default function Footer() {
           </div>
         </div>
         <div className='flex items-center gap-6 md:hidden justify-center pt-7 pb-4'>
-          <Link href='https://www.facebook.com/PunkgaMeManga/'>
+          <Link target='_blank' href='https://www.facebook.com/PunkgaMeManga/'>
             <svg xmlns='http://www.w3.org/2000/svg' width='32' height='33' viewBox='0 0 32 33' fill='none'>
               <circle cx='16' cy='16.8906' r='14' fill='#23FF81' />
               <path
@@ -119,7 +119,7 @@ export default function Footer() {
               />
             </svg>
           </Link>
-          <Link href='https://twitter.com/PunkgaMeManga'>
+          <Link target='_blank' href='https://twitter.com/PunkgaMeManga'>
             <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32' fill='none'>
               <path
                 d='M2.06813 3L12.8786 17.4544L2 29.2065H4.44853L13.9729 18.9171L21.6681 29.2065H30L18.581 13.9392L28.7069 3H26.2584L17.4872 12.476L10.4 3H2.06813ZM5.6688 4.80339H9.49643L26.3989 27.4031H22.5712L5.6688 4.80339Z'
@@ -127,7 +127,7 @@ export default function Footer() {
               />
             </svg>
           </Link>
-          <Link href='https://www.youtube.com/@PunkgaMeManga'>
+          <Link target='_blank' href='https://www.youtube.com/@PunkgaMeManga'>
             <svg xmlns='http://www.w3.org/2000/svg' width='32' height='33' viewBox='0 0 32 33' fill='none'>
               <path
                 d='M2.24451 10.8317C2.37304 8.85296 3.96395 7.30219 5.94447 7.20407C8.81239 7.06199 12.9115 6.89062 16 6.89062C19.0885 6.89062 23.1876 7.06199 26.0555 7.20407C28.0361 7.30219 29.627 8.85296 29.7555 10.8317C29.8786 12.7276 30 15.0603 30 16.8906C30 18.721 29.8786 21.0537 29.7555 22.9495C29.627 24.9283 28.0361 26.4791 26.0555 26.5772C23.1876 26.7193 19.0885 26.8906 16 26.8906C12.9115 26.8906 8.81239 26.7193 5.94447 26.5772C3.96395 26.4791 2.37304 24.9283 2.24451 22.9495C2.12136 21.0537 2 18.721 2 16.8906C2 15.0603 2.12136 12.7276 2.24451 10.8317Z'
