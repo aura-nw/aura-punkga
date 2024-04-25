573e98f884d3e221d9b798588b5aeee0cc1b5416
imhson <phamhoangson99@gmail.com>
2024-04-25 16:34:22 +0700

    update: ui/ux for setting new password modal

--- src/components/pages/profile/settingPasswordModal.tsx
+++ src/components/pages/profile/settingPasswordModal.tsx
@@ -4,14 +4,20 @@ import Modal from 'components/Modal'
 import Image from 'next/image'
 import { useContext, useEffect, useRef, useState } from 'react'
 import { Context } from 'src/context'
-import SuccessImg from 'images/ninja.svg'
+import SuccessImg from 'images/Mascot2.png'
 import { validatePassword } from 'src/utils'
 import CheckSquare from 'images/icons/check_square_fill.svg'
 import { useTranslation } from 'react-i18next'
 import _ from 'lodash'
+import Eye from 'assets/images/icons/Eye.svg'
+import EyeClosed from 'assets/images/icons/eye_closed.svg'
 export default function SettingPasswordModal({ open, setOpen, profile }) {
   const [success, setSuccess] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [inputVisiblity, setInputVisiblity] = useState({
+    newPassword: false,
+    confirmNewPassword: false,
+  })
   const [newPassword, setNewPassword] = useState('')
   const [rePassword, setRePassword] = useState('')
   const [rePasswordError, setRePasswordError] = useState('')
@@ -70,38 +76,71 @@ export default function SettingPasswordModal({ open, setOpen, profile }) {
   }
 
   return (
-    <Modal open={open} setOpen={setOpen} hideClose={success}>
-      <div className={`p-6 w-[322px] relative transition-all duration-300 ${success ? 'h-[400px]' : ''}`}>
-        <div className={` flex flex-col gap-3 transition-all duration-300 ${success ? 'opacity-0' : 'opacity-100'}`}>
+    <Modal open={open} setOpen={setOpen} hideClose={success} preventClickOutsideToClose>
+      <div className={`p-6 w-[322px] relative transition-all duration-300 ${success ? 'h-[380px]' : ''}`}>
+        <div className={` flex flex-col gap-6 transition-all duration-300 ${success ? 'opacity-0' : 'opacity-100'}`}>
           <p className='text-center text-xl leading-6 font-semibold'>{t('Set password')}</p>
-          <OutlineTextField
-            label={t('Password')}
-            value={newPassword}
-            onChange={setNewPassword}
-            type='password'
-            placeholder={t('Enter new password')}
-            onKeyDown={(e) => {
-              if (e.which == 13) {
-                r1.current?.focus()
+          <div>
+            <OutlineTextField
+              label={t('Password')}
+              value={newPassword}
+              onChange={setNewPassword}
+              type={inputVisiblity.newPassword ? 'text' : 'password'}
+              trailingComponent={
+                inputVisiblity.newPassword ? (
+                  <Image
+                    src={Eye}
+                    alt=''
+                    onClick={() => setInputVisiblity((prev) => ({ ...prev, newPassword: false }))}
+                    className='cursor-pointer'
+                  />
+                ) : (
+                  <Image
+                    src={EyeClosed}
+                    alt=''
+                    onClick={() => setInputVisiblity((prev) => ({ ...prev, newPassword: true }))}
+                    className='cursor-pointer'
+                  />
+                )
               }
-            }}
-          />
-          <OutlineTextField
-            label={t('Confirm password')}
-            value={rePassword}
-            onChange={setRePassword}
-            type='password'
-            errorMsg={rePasswordError}
-            placeholder={t('Re-Enter new password')}
-            inputRef={r1}
-            trailingComponent={repasswordValidateSuccess ? <Image src={CheckSquare} alt='' /> : null}
-          />
-          <FilledButton
-            disabled={!newPassword || !rePassword}
-            className='mt-2 mx-auto'
-            size='lg'
-            loading={loading}
-            onClick={setPasswordHandler}>
+              placeholder={t('Enter new password')}
+              onKeyDown={(e) => {
+                if (e.which == 13) {
+                  r1.current?.focus()
+                }
+              }}
+            />
+            <OutlineTextField
+              label={t('Confirm password')}
+              value={rePassword}
+              onChange={setRePassword}
+              type={inputVisiblity.confirmNewPassword ? 'text' : 'password'}
+              trailingComponent={
+                <div className='flex items-center gap-[10px]'>
+                  {repasswordValidateSuccess ? <Image src={CheckSquare} alt='' /> : null}
+                  {inputVisiblity.confirmNewPassword ? (
+                    <Image
+                      src={Eye}
+                      alt=''
+                      onClick={() => setInputVisiblity((prev) => ({ ...prev, confirmNewPassword: false }))}
+                      className='cursor-pointer'
+                    />
+                  ) : (
+                    <Image
+                      src={EyeClosed}
+                      alt=''
+                      onClick={() => setInputVisiblity((prev) => ({ ...prev, confirmNewPassword: true }))}
+                      className='cursor-pointer'
+                    />
+                  )}
+                </div>
+              }
+              errorMsg={rePasswordError}
+              placeholder={t('Re-Enter new password')}
+              inputRef={r1}
+            />
+          </div>
+          <FilledButton disabled={!newPassword || !rePassword} loading={loading} onClick={setPasswordHandler}>
             {t('Confirm')}
           </FilledButton>
         </div>
@@ -109,18 +148,16 @@ export default function SettingPasswordModal({ open, setOpen, profile }) {
           className={`absolute inset-0 py-6 px-4 flex flex-col gap-4 transition-all duration-300 ${
             success ? 'opacity-100' : 'opacity-0 pointer-events-none'
           }`}>
-          <p className='text-center text-xl leading-6 font-semibold'>{t('Password set')}!</p>
+          <p className='text-center text-xl leading-6 font-bold'>{t('Password set')}!</p>
           <Image src={SuccessImg} alt='' className='mx-auto h-[188px]' />
-          <p className='text-sm leading-6 font-medium text-center w-[246px] mx-auto'>
-            {t('You can now use the new password to sign in to your account')}
+          <p className='text-sm leading-[18px] font-medium text-center w-[274px] mx-auto text-[#414141]'>
+            {t('You can use the new password to log in Punkga now')}
           </p>
           <FilledButton
-            className='-mt-1 mx-auto'
-            size='lg'
             onClick={() => {
               setOpen(false)
             }}>
-            {t('Continue')}
+            {t('Confirm')}
           </FilledButton>
         </div>
       </div>
