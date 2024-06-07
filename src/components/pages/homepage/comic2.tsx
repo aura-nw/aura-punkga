4f5d7e030ed77f8c2f3e513f70aa1454fed6dcfb
AURA-QUANNN7\QUANNN7 <quannguyen2724@gmail.com>
2024-06-07 11:05:57 +0700

    new UI for comic card in homepage

---
+++ src/components/pages/homepage/comic2.tsx
@@ -0,0 +1,77 @@
+import NoImage from 'images/no_img.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { IComic } from 'src/models/comic'
+import { ArrowRightIcon, BellAlertIcon, BellIcon, EyeIcon, HeartIcon } from '@heroicons/react/20/solid';
+
+export default function Comic2(props: IComic) {
+    const { locale } = useRouter()
+    const router = useRouter()
+    const { t } = useTranslation()
+    const infoRef = useRef()
+    const [lineClamp, setLineClamp] = useState(0)
+    useEffect(() => {
+        const height = (infoRef?.current as any)?.getBoundingClientRect()?.height
+        const res = Math.floor((200 - height) / 20)
+        if (res >= 1) {
+            setLineClamp(res > 3 ? 3 : res)
+        }
+    }, [])
+    return (
+        <div className={`${props.status.text == 'Upcoming' ? '[&_a:not(.author)]:pointer-events-none' : ''}`}>
+            <div className='h-[230px]'>
+                <Link href={`/comic/${props.slug}`} className='relative w-full h-full aspect-[160/230] mx-auto'>
+                    <div className='block h-full'>
+                        <Image
+                            src={props.image || NoImage}
+                            alt=''
+                            width={180}
+                            height={240}
+                            className={`w-full h-full ${props.image ? 'object-cover' : 'object-contain bg-light-gray'
+                                } rounded-t `}
+                        />
+                    </div>
+                    <div className="absolute overflow-hidden bottom-0 w-full h-[50px] p-2  transition-all duration-300 ease-in-out hover:h-full hover:bg-gradient-to-t from-black to-transparent"
+                        style={{ background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.75) 13.66%)' }}>
+
+                        <p className='text-second-color whitespace-nowrap text-ellipsis overflow-hidden text-base font-bold leading-5'>{props[locale].title}</p>
+                        <div className='flex text-white gap-1 whitespace-nowrap text-ellipsis overflow-hidden text-sm font-semibold leading-[17px]'>
+                            {t('by')}{' '}
+                            {props.authors.map((author, index) => (
+                                <Fragment key={index}>
+                                    <span className='text-second-color font-[600] first:hidden'>, </span>
+                                    <span className='text-second-color font-[600]'>
+                                        {author.slug ? (
+                                            <div className='author'>
+                                                {t(author.name)}
+                                            </div>
+                                        ) : (
+                                            t(author.name)
+                                        )}
+                                    </span>
+                                </Fragment>
+                            ))}
+                        </div>
+                        <div>
+                            <div className="flex gap-1 items-center text-sm mt-1 font-semibold text-white">
+                                <div className="flex items-end gap-1">
+                                    <span>{props.views}</span>
+                                    <EyeIcon className="w-5 h-5" />
+                                </div>
+                                <div className="flex items-end gap-1">
+                                    <span>{props.likes}</span>
+                                    <HeartIcon className="w-5 h-5" />
+                                </div>
+                            </div>
+                                <div className='text-xs leading-4 mt-3 text-white'>{props[locale].description}</div>
+                
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+}
