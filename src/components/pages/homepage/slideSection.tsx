566f9cf095a425ff0e65fbab5e297512f50e0d25
Hoang Son Pham <37436325+imhson@users.noreply.github.com>
2024-06-15 11:29:05 +0700

    update banner

--- src/components/pages/homepage/slideSection.tsx
+++ src/components/pages/homepage/slideSection.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Slider from 'react-slick'
-
+import { useRouter } from 'next/router'
 import Banner from './assets/banner.png'
+
+import BannerEn from 'components/pages/event/assets/banner_en.png'
 import Event from './assets/event-mission.png'
 import { list } from './comicSlider'
 function Carousel({ children, className, sliderNavRef }) {
@@ -19,10 +21,11 @@ function Carousel({ children, className, sliderNavRef }) {
 }
 
 export default function SlideSection({ sliderNavRef }) {
+  const { locale } = useRouter()
   return (
     <>
       <div className='relative w-full rounded-2xl overflow-hidden hidden md:block'>
-        <Carousel sliderNavRef={sliderNavRef} className=''>
+        {/* <Carousel sliderNavRef={sliderNavRef} className=''>
           {[...list.slice(1), list[0]].map((data, index) => (
             <div key={index} className='outline-none [&_*]:outline-none'>
               <Link href={data.comic.href} className='relative'>
@@ -36,10 +39,10 @@ export default function SlideSection({ sliderNavRef }) {
               </Link>
             </div>
           ))}
-        </Carousel>
-        {/* <Link href='/campaigns'>
-          <Image src={Event} alt='' className='rounded-[10px] w-full aspect-[40/15] object-cover' />
-        </Link> */}
+        </Carousel> */}
+        <Link href='/campaigns'>
+          <Image src={locale == 'vn' ? Event : BannerEn} alt='' className='rounded-[10px] w-full aspect-[40/15] object-cover' />
+        </Link>
       </div>
       <div className='px-5 md:px-0'>
         <div className='mt-5 rounded-2xl flex flex-col gap-[10px]'>
