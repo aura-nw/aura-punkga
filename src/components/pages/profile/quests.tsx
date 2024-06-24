2b91cd6059a59b3c0cc65ba1f9dc5834aa4d90b5
AURA-QUANNN7\QUANNN7 <quannguyen2724@gmail.com>
2024-06-24 18:54:44 +0700

    hotfix: profile campaign width

--- src/components/pages/profile/quests.tsx
+++ src/components/pages/profile/quests.tsx
@@ -33,84 +33,89 @@ export default function QuestSection() {
       {!!data?.length ? (
         <>
           <div className='w-full relative mt-[47px] hidden xl:block'>
-            <div className='block [&_.swiper-button-prev]:text-[#61646B] [&_.swiper-button-next]:text-[#61646B] items-center'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='37'
-                height='36'
-                viewBox='0 0 37 36'
-                fill='none'
-                className='shrink-0 cursor-pointer swiper-prev mr-5'>
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M23.7321 6.64586C24.2039 7.05021 24.2585 7.76042 23.8542 8.23216L15.4817 18L23.8542 27.7679C24.2585 28.2396 24.2039 28.9498 23.7321 29.3542C23.2604 29.7585 22.5502 29.7039 22.1458 29.2322L13.1458 18.7322C12.7847 18.3109 12.7847 17.6892 13.1458 17.2679L22.1458 6.76788C22.5502 6.29614 23.2604 6.24151 23.7321 6.64586Z'
-                  fill='#61646B'
-                />
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M23.7321 6.64586C24.2039 7.05021 24.2585 7.76042 23.8542 8.23216L15.4817 18L23.8542 27.7679C24.2585 28.2396 24.2039 28.9498 23.7321 29.3542C23.2604 29.7585 22.5502 29.7039 22.1458 29.2322L13.1458 18.7322C12.7847 18.3109 12.7847 17.6892 13.1458 17.2679L22.1458 6.76788C22.5502 6.29614 23.2604 6.24151 23.7321 6.64586Z'
-                  fill='black'
-                  fillOpacity='0.2'
-                />
-              </svg>
-              <Swiper
-                autoplay={{
-                  delay: 4000,
-                }}
-                loop
-                navigation={{
-                  nextEl: '.swiper-next',
-                  prevEl: '.swiper-prev',
-                }}
-                breakpoints={{
-                  1536: {
-                    slidesPerView: 2,
-                  },
-                }}
-                spaceBetween={20}
-                slidesPerView={1}
-                modules={[Navigation]}>
-                {data?.map((quest: Quest) => {
-                  return (
-                    <SwiperSlide key={`${quest.id}`}>
-                      <QuestItem
-                        quest={quest}
-                        refreshCallback={() =>
-                          setTimeout(() => {
-                            mutate({ key: 'get_available_quests', account: account?.id })
-                            mutate('get_leaderboard')
-                            getProfile()
-                          }, 2000)
-                        }
-                      />
-                    </SwiperSlide>
-                  )
-                })}
-              </Swiper>
+            <div className='block'>
+              <div className='flex [&_.swiper-button-prev]:text-[#61646B] [&_.swiper-button-next]:text-[#61646B] items-center'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='37'
+                  height='36'
+                  viewBox='0 0 37 36'
+                  fill='none'
+                  className='shrink-0 cursor-pointer swiper-prev mr-5'>
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M23.7321 6.64586C24.2039 7.05021 24.2585 7.76042 23.8542 8.23216L15.4817 18L23.8542 27.7679C24.2585 28.2396 24.2039 28.9498 23.7321 29.3542C23.2604 29.7585 22.5502 29.7039 22.1458 29.2322L13.1458 18.7322C12.7847 18.3109 12.7847 17.6892 13.1458 17.2679L22.1458 6.76788C22.5502 6.29614 23.2604 6.24151 23.7321 6.64586Z'
+                    fill='#61646B'
+                  />
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M23.7321 6.64586C24.2039 7.05021 24.2585 7.76042 23.8542 8.23216L15.4817 18L23.8542 27.7679C24.2585 28.2396 24.2039 28.9498 23.7321 29.3542C23.2604 29.7585 22.5502 29.7039 22.1458 29.2322L13.1458 18.7322C12.7847 18.3109 12.7847 17.6892 13.1458 17.2679L22.1458 6.76788C22.5502 6.29614 23.2604 6.24151 23.7321 6.64586Z'
+                    fill='black'
+                    fillOpacity='0.2'
+                  />
+                </svg>
+                <div className='max-w-[calc(100%-112px)]'>
+                  <Swiper
+                    autoplay={{
+                      delay: 4000,
+                    }}
+                    loop
+                    navigation={{
+                      nextEl: '.swiper-next',
+                      prevEl: '.swiper-prev',
+                    }}
+                    breakpoints={{
+                      1536: {
+                        slidesPerView: 2,
+                      },
+                    }}
+                    spaceBetween={20}
+                    slidesPerView={1}
+                    modules={[Navigation]}>
+                    {data?.map((quest: Quest) => {
+                      return (
+                        <SwiperSlide key={`${quest.id}`}>
+                          <QuestItem
+                            quest={quest}
+                            refreshCallback={() =>
+                              setTimeout(() => {
+                                mutate({ key: 'get_available_quests', account: account?.id })
+                                mutate('get_leaderboard')
+                                getProfile()
+                              }, 2000)
+                            }
+                          />
+                        </SwiperSlide>
+                      )
+                    })}
+                  </Swiper>
+                </div>
 
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='37'
-                height='36'
-                viewBox='0 0 37 36'
-                fill='none'
-                className='shrink-0 cursor-pointer swiper-next ml-5'>
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M13.2679 6.64586C13.7396 6.24151 14.4498 6.29614 14.8542 6.76788L23.8542 17.2679C24.2153 17.6892 24.2153 18.3109 23.8542 18.7322L14.8542 29.2322C14.4498 29.7039 13.7396 29.7585 13.2679 29.3542C12.7961 28.9498 12.7415 28.2396 13.1459 27.7679L21.5183 18L13.1459 8.23216C12.7415 7.76042 12.7961 7.05021 13.2679 6.64586Z'
-                  fill='#61646B'
-                />
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M13.2679 6.64586C13.7396 6.24151 14.4498 6.29614 14.8542 6.76788L23.8542 17.2679C24.2153 17.6892 24.2153 18.3109 23.8542 18.7322L14.8542 29.2322C14.4498 29.7039 13.7396 29.7585 13.2679 29.3542C12.7961 28.9498 12.7415 28.2396 13.1459 27.7679L21.5183 18L13.1459 8.23216C12.7415 7.76042 12.7961 7.05021 13.2679 6.64586Z'
-                  fill='black'
-                  fillOpacity='0.2'
-                />
-              </svg>
+
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='37'
+                  height='36'
+                  viewBox='0 0 37 36'
+                  fill='none'
+                  className='shrink-0 cursor-pointer swiper-next ml-5'>
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M13.2679 6.64586C13.7396 6.24151 14.4498 6.29614 14.8542 6.76788L23.8542 17.2679C24.2153 17.6892 24.2153 18.3109 23.8542 18.7322L14.8542 29.2322C14.4498 29.7039 13.7396 29.7585 13.2679 29.3542C12.7961 28.9498 12.7415 28.2396 13.1459 27.7679L21.5183 18L13.1459 8.23216C12.7415 7.76042 12.7961 7.05021 13.2679 6.64586Z'
+                    fill='#61646B'
+                  />
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M13.2679 6.64586C13.7396 6.24151 14.4498 6.29614 14.8542 6.76788L23.8542 17.2679C24.2153 17.6892 24.2153 18.3109 23.8542 18.7322L14.8542 29.2322C14.4498 29.7039 13.7396 29.7585 13.2679 29.3542C12.7961 28.9498 12.7415 28.2396 13.1459 27.7679L21.5183 18L13.1459 8.23216C12.7415 7.76042 12.7961 7.05021 13.2679 6.64586Z'
+                    fill='black'
+                    fillOpacity='0.2'
+                  />
+                </svg>
+              </div>
             </div>
             <Link
               className='text-second-color underline font-bold px-6 w-full text-center block mt-[10px]'
