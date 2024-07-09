dbbde48f5b674a78caa593cbfd98bfca6bee0863
imhson <phamhoangson99@gmail.com>
2024-07-09 10:02:43 +0700

    update color

--- src/components/core/Chip/index.tsx
+++ src/components/core/Chip/index.tsx
@@ -2,17 +2,23 @@ import { ReactNode } from 'react'
 
 export default function Chip({
   children,
-  color,
-  onClick
+  color = 'default',
+  onClick,
 }: {
   children: ReactNode | JSX.Element
   color: 'success' | 'warning' | 'error' | 'process' | 'default'
   onClick?: () => any
 }) {
   const colorCn = {
-    success: ''
+    success: 'text-text-success-primary',
+    warning: 'text-text-warning-primary-2',
+    error: 'text-text-error-primary-3',
+    process: 'text-text-info-primary',
+    default: 'text-text-teriary',
   }
-  return <div onClick={onClick} className={`text-center text-sm font-medium leading-5 text-`}>
-    {children}
-  </div>
+  return (
+    <div onClick={onClick} className={`text-center text-sm font-medium leading-5`}>
+      {children}
+    </div>
+  )
 }
