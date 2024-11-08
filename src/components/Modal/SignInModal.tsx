a3d2698699a0b620feb56eb38d7a9dd676cfe659
Hoang Son Pham <37436325+imhson@users.noreply.github.com>
2024-11-08 19:28:22 +0700

    Update SignInModal.tsx

--- src/components/Modal/SignInModal.tsx
+++ src/components/Modal/SignInModal.tsx
@@ -64,7 +64,7 @@ export default function SignInModal() {
             <>
               <div className='flex-1'>
                 <div className='font-semibold text-[#414141]'>{t('Under maintenance')}</div>
-                //<ByWallet step={step} />
+                {/* <ByWallet step={step} /> */}
               </div>
               <div className='w-[1px] h-[400px] bg-[#F0F0F0] hidden md:block'></div>
               <div className='md:hidden flex gap-[18px] items-center w-full'>
