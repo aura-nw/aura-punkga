466aae6fa4b13e6143726b018dddb8651975f9d4
imhson <phamhoangson99@gmail.com>
2024-11-29 11:27:40 +0700

    update event metadata

--- src/pages/events/your-city/index.tsx
+++ src/pages/events/your-city/index.tsx
@@ -2,6 +2,7 @@ import HeadComponent from 'components/Head'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Layout from 'components/Layout'
 import Event from './page'
+import { pageMetadata } from '..'
 
 export default function Page(props) {
   if (props.justHead || props.pageProps?.justHead) {
@@ -18,23 +19,9 @@ Page.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>
 }
 export const getServerSideProps = async (context) => {
-  const props = {
-    image:
-      context.locale == 'vn'
-        ? 'https://punkga.me/assets/images/ava-thumb.png'
-        : 'https://punkga.me/assets/images/ava-thumb.png',
-    title:
-      context.locale == 'vn'
-        ? `AVA Grand Contest 2024 - Find Your Artistic Voice`
-        : `AVA Grand Contest 2024 - Find Your Artistic Voice`,
-    description:
-      context.locale == 'vn'
-        ? `Tham gia AVA Grand Contest 2024 trên PunkgaMe để chứng tỏ bản sắc nghệ thuật của riêng bạn và giành lấy phần thưởng lên tới 150 Triệu VNĐ!`
-        : `Join the AVA Grand Contest 2024 on Punkga Me to showcase your talent and win prizes up to 6,000 USD. Prove the power of true art in an AI-driven world!`,
-  }
   return {
     props: {
-      metadata: props,
+      metadata: pageMetadata(context),
       ...(await serverSideTranslations(context?.locale!, ['common'])),
     },
   }
