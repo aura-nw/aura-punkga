df3d3bfaf9e2683eb67b2f8893fa3c325a4ac32f
imhson <phamhoangson99@gmail.com>
2024-12-19 15:05:19 +0700

    update activities

--- src/components/Header/components/UserDropdown.tsx
+++ src/components/Header/components/UserDropdown.tsx
@@ -83,7 +83,7 @@ export default function UserDropdown() {
                   Activity
                 </div>
                 <div className='mt-4 space-y-4 h-[400px] overflow-auto'>
-                  {account.activities.map((act, index) => {
+                  {account?.activities?.map((act, index) => {
                     if (act.type == 'SUBMIT_ARTWORK')
                       return (
                         <div key={index} className='flex gap-4 items-center'>
