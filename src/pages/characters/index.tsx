fbb3ef241347db3ea4feb97282851833f9cb6122
Sunday <phamhoangson99@gmail.com>
2025-02-05 11:19:05 +0700

    update metadata

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -19,6 +19,32 @@ Page.getLayout = function getLayout(page) {
 }
 
 export const getServerSideProps = async (context) => {
+  const characterId = context.query.character_id
+  if (characterId) {
+    const host = context.req.headers.host || context.req.headers.Host
+    const res = await fetch(
+      host.includes('dev') || host.includes('localhost')
+        ? `https://api.dev.punkga.me/story-event/character/get-by-id/${characterId}`
+        : host.includes('staging')
+        ? `https://api.staging.punkga.me/story-event/character/get-by-id/${characterId}`
+        : `https://api.punkga.me/story-event/character/get-by-id/${characterId}`
+    )
+    const data = await res.json()
+    const characterData = data?.data?.story_character_by_pk
+    const props = {
+      title: characterData?.name || 'PunkgaMe: Colorful Adventures with Unique Characters',
+      description:
+        characterData?.description ||
+        'Explore a colorful world and embark on exciting adventures with unique characters created by talented artists!',
+      image: characterData?.avatar_url || '/assets/images/characters-thumbnail.png',
+    }
+    return {
+      props: {
+        metadata: props,
+        ...(await serverSideTranslations(context?.locale!, ['common'])),
+      },
+    }
+  }
   return {
     props: {
       metadata: {
